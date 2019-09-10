d3144efddb4f98c435bed67d2dd11a916592e815
Ian Christie <ichristi231@gmail.com>
2019-09-10 15:50:57 -0500

    fixed indentation

--- leptonic.c
+++ leptonic.c
@@ -72,14 +72,14 @@ double interpolation(double xx,double x1, double x2, double y1, double y2);
 /* See eqn. 4.7 in Coppi & Blandford '90. */
 double photon_annihilation_rate(double o)
 {
-    if (o >= 1.) 
-    {
-        return 0.652 * sigmaT * c * log(o) * (o * o - 1.) / pow(o, 3.);
-    }
-    else 
-    {
-        return 0.;
-    }
+  if (o >= 1.) 
+  {
+    return 0.652 * sigmaT * c * log(o) * (o * o - 1.) / pow(o, 3.);
+  }
+  else 
+  {
+    return 0.;
+  }
 
 }
 /**********************************************************************/
@@ -194,11 +194,10 @@ double plasmoid_size[number_of_elements];
 
 for (i = 0; i < number_of_elements; i++)
 {
-    fscanf(plasmoid_Lorentz_file, "%lf", &plasmoid_Lorentz_factor[i]);
-    fscanf(plasmoid_volume_file, "%lf", &plasmoid_volume[i]);
-    fscanf(plasmoid_volume_derivative_file, "%lf", &plasmoid_volume_derivative[i]);
-    fscanf(plamoid_size_file, "%lf", &plasmoid_size[i]);
-
+  fscanf(plasmoid_Lorentz_file, "%lf", &plasmoid_Lorentz_factor[i]);
+  fscanf(plasmoid_volume_file, "%lf", &plasmoid_volume[i]);
+  fscanf(plasmoid_volume_derivative_file, "%lf", &plasmoid_volume_derivative[i]);
+  fscanf(plamoid_size_file, "%lf", &plasmoid_size[i]);
 }
 
 fclose(plasmoid_Lorentz_file);
@@ -220,7 +219,7 @@ proton_mass = 1.6726219E-24;
 electric_charge = 4.8032068E-10; 
 /* Plank's constant in erg s. */
 plank_const = 6.6260755E-27;
-/* Boltzman_constman constant in erg K^(-1). */
+/* Boltzman constant in erg K^(-1). */
 boltzman_const = 1.380658E-16;
 /***************************************************/
 /***************************************************/
@@ -240,8 +239,8 @@ xt = fopen("required_tables/single_part_emissivity_x_store.txt", "r");
 Ft = fopen("required_tables/single_part_emissivity_f_store.txt", "r"); 
 for (i = 0; i < 400; i++) 
 {
-    fscanf(xt, "%lf", &x_store_table[i]); 
-    fscanf(Ft, "%lf", &f_store_table[i]);
+  fscanf(xt, "%lf", &x_store_table[i]); 
+  fscanf(Ft, "%lf", &f_store_table[i]);
 }
 fclose(xt); fclose(Ft);
 /******************************************************************************/
@@ -259,9 +258,9 @@ double nu[lmax], x[lmax], delta_x;
 
 for (l = 0; l < lmax; l++) 
 {
-    nu[l] = pow(10, 7. + (l / (lmax - 1.)) * (40. - 7.)); 
-    x[l] = plank_const * nu[l] / (electron_mass * c * c); 
-    fprintf(nu_save, "%lf\n", log10(nu[l]));
+  nu[l] = pow(10, 7. + (l / (lmax - 1.)) * (40. - 7.)); 
+  x[l] = plank_const * nu[l] / (electron_mass * c * c); 
+  fprintf(nu_save, "%lf\n", log10(nu[l]));
 }
 fclose(nu_save);
 /* The difference of the log-space spacing between photon energies.
@@ -308,16 +307,18 @@ int k;
    eqn. A4 in Christie et al. 2019. */ 
 ge_min = (electron_injec_slope - 2.) * (1. + 4. * magnetization * proton_mass /
     (plasmoid_avg_num_den * electron_mass * pair_multiplicity)) / (electron_injec_slope - 1.);
+printf("Minimum Lorentz factor: %f. Maximum Lorentz factor: %f. \n", ge_min, ge_max);
 
 /* Lorentz factor of the electron distribution, 
 evaluated at the half grid pints, and the spacing between 
 each of them as governed by Chiaberge & Ghisellini '99. */
-for (k = 1; k < kmax + 1; k++){
+for (k = 1; k < kmax + 1; k++)
+{
 	ge[k - 1] = 1.01 * pow(ge_max / 1.01, (k - 1.) / (kmax - 1.));
-    ge_minus[k - 1] = 1.01 * pow(ge_max / 1.01, (k - 0.5 - 1.) / (kmax - 1.));
-    ge_plus[k - 1] = 1.01 * pow(ge_max / 1.01, (k + 0.5 - 1.) / (kmax - 1.));
-    delta_ge[k - 1] = ge_plus[k - 1] - ge_minus[k - 1];
-    fprintf(gamma_e_save, "%lf\n", log10(ge[k - 1]));
+  ge_minus[k - 1] = 1.01 * pow(ge_max / 1.01, (k - 0.5 - 1.) / (kmax - 1.));
+  ge_plus[k - 1] = 1.01 * pow(ge_max / 1.01, (k + 0.5 - 1.) / (kmax - 1.));
+  delta_ge[k - 1] = ge_plus[k - 1] - ge_minus[k - 1];
+  fprintf(gamma_e_save, "%lf\n", log10(ge[k - 1]));
 }
 fclose(gamma_e_save);
 /* The difference of the log-space spacing between particle energies.
@@ -341,21 +342,21 @@ we find the closest dimensionless photon frequency x-value within the array such
 perform the summation of the inverse Compton (Thomson regime) source term. */
 for (l = 0; l < lmax; l++)
 {
-    if (3. * x[l] / 4. > 3. / (4. * x[l])) 
-    {
-        ict_bound[l] = 3. / (4. * x[l]);
-    }
-    else 
-    {
-        ict_bound[l] = 3. * x[l] / 4.;
-    }
+  if (3. * x[l] / 4. > 3. / (4. * x[l])) 
+  {
+    ict_bound[l] = 3. / (4. * x[l]);
+  }
+  else 
+  {
+    ict_bound[l] = 3. * x[l] / 4.;
+  }
 
-	sum = 0;
-    for (ll = 0; ll < lmax; ll++) 
-    {
-        sum += (ict_bound[l] >= x[ll]);
-    }
-    ict_bound_index[l] = sum;
+  sum = 0;
+  for (ll = 0; ll < lmax; ll++) 
+  {
+    sum += (ict_bound[l] >= x[ll]);
+  }
+  ict_bound_index[l] = sum;
 }
 
 /* Below, we determine the index of the dimensionless photon frequency array which is closest
@@ -365,12 +366,12 @@ for (l = 0; l < lmax; l++)
 for (k = 0; k < kmax; k++)
 {
 	ict_bound_loss_plus[k] = 0; 
-    ict_bound_loss_minus[k] = 0;
+  ict_bound_loss_minus[k] = 0;
 	for (l = 0; l < lmax; l++) 
-    {
-        ict_bound_loss_plus[k] += (3. / (4. * ge_plus[k])) >= x[l]; 
-        ict_bound_loss_minus[k] += (3. / (4. * ge_minus[k])) >= x[l];
-    }
+  {
+    ict_bound_loss_plus[k] += (3. / (4. * ge_plus[k])) >= x[l]; 
+    ict_bound_loss_minus[k] += (3. / (4. * ge_minus[k])) >= x[l];
+  }
 }
 
 /* Below, we determine for which values of the dimensionless photon frequency fall within the range 
@@ -385,9 +386,9 @@ for (l = 0; l < lmax; l++)
 		if (ict_Ne_gamma[l][ll] == 1)
 		{
 			for (k = 0; k < kmax; k++) 
-            {
-                sum += (sqrt(3. * x[l] / (4. * x[ll])) >= ge[k]);
-            }
+      {
+        sum += (sqrt(3. * x[l] / (4. * x[ll])) >= ge[k]);
+      }
 			ict_Ne_gamma_index[l][ll] = sum;
 		}
 	}
@@ -407,9 +408,9 @@ for (k = 0; k < kmax; k++)
 {
 	ickn_bound_loss[k] = 0;
 	for (l = 0; l < lmax; l++) 
-    {
-        ickn_bound_loss[k] += (3. / (4. * ge[k])) >= x[l];
-    }
+  {
+    ickn_bound_loss[k] += (3. / (4. * ge[k])) >= x[l];
+  }
 }
 
 /* Below, we determine the index in the dimensionless photon frequency array in order to determine
@@ -419,9 +420,9 @@ for (l = 0; l < lmax; l++)
 {
 	ickn_bound_source[l] = 0;
 	for (ll = 0; ll < lmax; ll++) 
-    {
-        ickn_bound_source[l] += (3. / (4. * x[l])) >= x[ll];
-    }
+  {
+    ickn_bound_source[l] += (3. / (4. * x[l])) >= x[ll];
+  }
 }
 
 /* Below, we determine the index within the electron Lorentz factor array which is closest to
@@ -431,12 +432,12 @@ for (l = 0; l < lmax; l++)
 {
 	ickn_gamma_source[l] = 0;
 	if (x[l] >= 1. && x[l] <= ge_max) 
+  {
+    for (k = 0; k < kmax; k++) 
     {
-        for (k = 0; k < kmax; k++) 
-        {
-            ickn_gamma_source[l] += x[l] >= ge[k];
-        }
+      ickn_gamma_source[l] += x[l] >= ge[k];
     }
+  }
 }
 /***************************************************************************************************/
 /***************************************************************************************************/
@@ -458,23 +459,26 @@ const_syn_e = 4. * sigmaT * UB / (3. * electron_mass *c);
    in Rybicki & Lightman '86. */
 for (l = 0; l < lmax; l++)
 {
-    for (k = 0; k < kmax; k++)
+  for (k = 0; k < kmax; k++)
+  {
+  	x_store = nu[l] * 4. * PI * electron_mass * c /(3. * electric_charge * B * ge[k] * ge[k]);
+
+  	if (x_store >= 1E-20 && x_store <= 100)
+  	{
+  		x_index = 0;
+  		for (i = 0; i < 400; i++) 
+      {
+        x_index += log10(x_store) >= x_store_table[i];
+      }
+  		f_store = pow(10, f_store_table[x_index]);
+  	}
+  	else 
     {
-    	x_store = nu[l] * 4. * PI * electron_mass * c /(3. * electric_charge * B * ge[k] * ge[k]);
-
-    	if (x_store >= 1E-20 && x_store <= 100)
-    	{
-    		x_index = 0;
-    		for (i = 0; i < 400; i++) 
-            {
-                x_index += log10(x_store) >= x_store_table[i];
-            }
-    		f_store = pow(10, f_store_table[x_index]);
-    	}
-    	else {f_store = 0;}
-
-    	pe_single[l][k] = sqrt(3.) * pow(electric_charge, 3.) * B * f_store / (electron_mass * c * c) ;
+      f_store = 0;
     }
+
+  	pe_single[l][k] = sqrt(3.) * pow(electric_charge, 3.) * B * f_store / (electron_mass * c * c) ;
+  }
 }
 /*********************************************************************************************/
 /*********************************************************************************************/
@@ -489,11 +493,13 @@ photon distribution at two times the electron Lorentz factor as stated in eqn. 5
 Mastichiadis & Kirk '95. */
 for (k = 0; k < kmax; k++)
 {
-    gg_ee_gamma_bound_source[k] = 0;
-    for (l = 0; l < lmax; l++) 
-    {
-        gg_ee_gamma_bound_source[k] += 2. * ge[k] >= x[l];
-    }
+  gg_ee_gamma_bound_source[k] = 0;
+  for (l = 0; l < lmax; l++) 
+  {
+    gg_ee_gamma_bound_source[k] += 2. * ge[k] >= x[l];
+  }
+  //printf("%d\n", gg_ee_gamma_bound_source[k]);
+
 }
 
 /* Below, we determine the index in the dimensionless photon frequency array in which 1/x = x, where
@@ -501,11 +507,11 @@ x is the dimensionless frequency. This index is used for the lower bound of the 
 by eqn. 54 in Mastichiadis & Kirk '95. */
 for (l = 0; l < lmax; l++)
 {
-    gg_ee_bound_loss[l] = 0;
-    for (ll = 0; ll < lmax; ll++) 
-    {
-        gg_ee_bound_loss[l] += (1. / x[l]) >= x[ll];
-    }
+  gg_ee_bound_loss[l] = 0;
+  for (ll = 0; ll < lmax; ll++) 
+  {
+    gg_ee_bound_loss[l] += (1. / x[l]) >= x[ll];
+  }
 }
 
 /* Below, we determine the index in the dimensionless frequency array in which the inverse of two
@@ -513,11 +519,12 @@ times the electron Lorentz factor is closest to the dimensionless frequency. Thi
 lower bound of the source term integral as governed by eqn. 57 in Mastichiadis & Kirk '95. */
 for (k = 0; k < kmax; k++)
 {
-    gg_ee_bound_source[k] = 0;
-    for (l = 0; l < lmax; l++) 
-    {
-        gg_ee_bound_source[k] += (1. / (2. * ge[k])) >= x[l];
-    }
+  gg_ee_bound_source[k] = 0;
+  for (l = 0; l < lmax; l++) 
+  {
+    gg_ee_bound_source[k] += (1. / (2. * ge[k])) >= x[l];
+  }
+  //printf("%d\n", gg_ee_bound_source[k]);
 }
 /*********************************************************************************************/
 /*********************************************************************************************/
@@ -631,14 +638,13 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 			for (ll = 0; ll < lmax; ll++)
 			{
 				if (ict_Ne_gamma[l][ll] == 1) 
-                {
-                    //Q_ICT_e[l] += c*sigmaT*sqrt(3.0/x[l])/(4.*vol)*delta_x*sqrt(x[ll])*(N_x[ll] + N_BLR[ll]*sw_ext)*N_e[ict_Ne_gamma_index[l][ll]];
-                    Q_ICT_e[l] += c * sigmaT * sqrt(3.0 / x[l]) / (4. * vol) * delta_x * sqrt(x[ll]) * 
-                    (N_x[ll]) * N_e[ict_Ne_gamma_index[l][ll]];
-                }
-			}
+        {
+          Q_ICT_e[l] += c * sigmaT * sqrt(3.0 / x[l]) / (4. * vol) * delta_x * sqrt(x[ll]) * 
+          (N_x[ll] + N_external[ll] * sw_ext) * N_e[ict_Ne_gamma_index[l][ll]];
         }
+			}
     }
+  }
 	/*********************************************************************************/
 	/*********************************************************************************/
 
@@ -693,13 +699,13 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 	{
 		Q_ICKN_e[l] = 0;
 		if (ickn_gamma_source[l] != 0 && ickn_bound_source[l] != 0) 
-        {
-            for (ll = ickn_bound_source[l]; ll < lmax; ll++) 
-            {
-                //Q_ICKN_e[l] += (sigmaT*c/vol)*(N_e[ickn_gamma_source[l]]/x[l])*delta_x*(N_x[ll] + N_BLR[ll]*sw_ext);
-                Q_ICKN_e[l] += (sigmaT * c / vol) * (N_e[ickn_gamma_source[l]] / x[l]) * delta_x * (N_x[ll]);
-            }
-        }
+    {
+      for (ll = ickn_bound_source[l]; ll < lmax; ll++) 
+      {
+        Q_ICKN_e[l] += (sigmaT * c / vol) * (N_e[ickn_gamma_source[l]] / x[l]) * delta_x * (N_x[ll] 
+          + N_external[ll] * sw_ext);
+      }
+    }
 	}
 	/************************************************************************************/
 	/************************************************************************************/
@@ -712,10 +718,10 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 	as determined from Chiaberge & Gisellini '99, eqn. 10. */
 	for (k = 0; k < kmax; k++)
 	{
-	    v2_e[k] = 1. + (delta_t / delta_ge[k]) * (ge_minus[k] * ge_minus[k]) * (const_syn_e*sw_syn_e + 
-            L_ICT_e_minus[k] * sw_ict);
-	    v3_e[k] = -(delta_t / delta_ge[k]) * (ge_plus[k] * ge_plus[k]) * (const_syn_e * sw_syn_e + 
-            L_ICT_e_plus[k] * sw_ict);
+    v2_e[k] = 1. + (delta_t / delta_ge[k]) * (ge_minus[k] * ge_minus[k]) * (const_syn_e*sw_syn_e + 
+      L_ICT_e_minus[k] * sw_ict);
+    v3_e[k] = -(delta_t / delta_ge[k]) * (ge_plus[k] * ge_plus[k]) * (const_syn_e * sw_syn_e + 
+      L_ICT_e_plus[k] * sw_ict);
 	}
 	/*****************************************************************/
 	/*****************************************************************/
@@ -726,14 +732,13 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 	/******************************************************/
 	for (l = 0; l < lmax; l++)
 	{
-        N_x[l] = N_x[l] * (1. - delta_t / t_esc) + delta_t * (Q_syn_e[l] * sw_syn_e + Q_ICT_e[l] * 
-            sw_ict + Q_ICKN_e[l] * sw_ickn);
-        //N_x[l] = N_x[l] * (1. - delta_t / t_esc) + delta_t * (Q_syn_e[l] * sw_syn_e + Q_ICT_e[l] * sw_ict);
+    N_x[l] = N_x[l] * (1. - delta_t / t_esc) + delta_t * (Q_syn_e[l] * sw_syn_e + Q_ICT_e[l] * 
+      sw_ict + Q_ICKN_e[l] * sw_ickn);
 		
-        if (N_x[l] < 0) 
-        {
-            N_x[l] = 1.E-200;
-        }
+    if (N_x[l] < 0) 
+    {
+      N_x[l] = 1.E-200;
+    }
 	}
 	/******************************************************/
 	/******************************************************/
@@ -792,9 +797,9 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 	{
 		N_x[l] -= delta_t * L_gg_ee[l] * (L_gg_ee[l] >= 0.) * sw_ggee;
 		if (N_x[l] < 0) 
-        {
-            N_x[l] = 1.E-200;
-        }
+    {
+      N_x[l] = 1.E-200;
+    }
 	}
 	/******************************************************/
 	/******************************************************/
@@ -806,30 +811,30 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 	for (k = kmax - 1; k >= 0; k--)
 	{
 		if (k == kmax - 1) 
-        {
-            N_e[k] = (N_e[k] + delta_t * (Qe_inj[k] - L_ICKN_e[k] * sw_ickn + Q_gg_ee[k] * sw_ggee)) / v2_e[k];
-        }
+    {
+      N_e[k] = (N_e[k] + delta_t * (Qe_inj[k] - L_ICKN_e[k] * sw_ickn + Q_gg_ee[k] * sw_ggee)) / v2_e[k];
+    }
 		if (k != kmax - 1) 
-        {
-            N_e[k] = (N_e[k] + delta_t * (Qe_inj[k] - L_ICKN_e[k] * sw_ickn + Q_gg_ee[k] * sw_ggee) - 
-                v3_e[k] * N_e[k + 1]) / v2_e[k];
-        }
+    {
+      N_e[k] = (N_e[k] + delta_t * (Qe_inj[k] - L_ICKN_e[k] * sw_ickn + Q_gg_ee[k] * sw_ggee) - 
+        v3_e[k] * N_e[k + 1]) / v2_e[k];
+    }
 		if (N_e[k] <= 0) 
-        {
-            N_e[k] = 1.E-20;
-        }
+    {
+      N_e[k] = 1.E-20;
+    }
 	}
 
 	for (k = 0; k < kmax; k++)
 	{
 		if (k != kmax - 1) 
-        {
-            fprintf(N_e_save, "%lf ", log10(pow(ge[k],electron_injec_slope)*N_e[k]) + 1.);
-        }
+    {
+      fprintf(N_e_save, "%lf ", log10(pow(ge[k],electron_injec_slope)*N_e[k]) + 1.);
+    }
 		else 
-        {
-            fprintf(N_e_save, "%lf\n", log10(pow(ge[k],electron_injec_slope)*N_e[k]) + 1.);
-        }
+    {
+      fprintf(N_e_save, "%lf\n", log10(pow(ge[k],electron_injec_slope)*N_e[k]) + 1.);
+    }
 	}
 	/********************************************************/
 	/********************************************************/
@@ -871,47 +876,47 @@ for (i = 0; i < (number_of_elements - 1) * sw_rtc; i++)
 		N_x[l] -= delta_t * L_ssa[l] * (L_ssa[l] >= 0.) * sw_ssa;
 
 		if (N_x[l] < 0) 
-        {
-            N_x[l] = 1.E-200;
-        }
+    {
+      N_x[l] = 1.E-200;
+    }
 
 		if (l != lmax - 1) 
-        {
-            fprintf(nu_L_nu_save, "%lf ", log10(N_x[l] * pow(plank_const * nu[l], 2.) / 
-                (electron_mass * c *c * t_esc) + 1.) );
-        }
+    {
+      fprintf(nu_L_nu_save, "%lf ", log10(N_x[l] * pow(plank_const * nu[l], 2.) / 
+        (electron_mass * c *c * t_esc) + 1.) );
+    }
 		else 
-        {
-            fprintf(nu_L_nu_save, "%lf\n", log10(N_x[l]*pow(plank_const * nu[l], 2.) / 
-                (electron_mass * c * c * t_esc) + 1.) );
-        }
+    {
+      fprintf(nu_L_nu_save, "%lf\n", log10(N_x[l]*pow(plank_const * nu[l], 2.) / 
+        (electron_mass * c * c * t_esc) + 1.) );
+    }
 	}
 	/******************************************************/
 	/******************************************************/
 
 
 
-    /******************* Checks Particle Number Conservation **************/
-    /**********************************************************************/
-    sum_inj = 0; N_tot = 0;
-    for (k = 0; k < kmax; k++)
-    {
-        N_tot += ge[k] * N_e[k] * delta_ge_int;
-        sum_inj += ge[k] * Qe_inj[k] * delta_ge_int;
-    }
-    N_tot_inj += delta_t * sum_inj;
+  /******************* Checks Particle Number Conservation **************/
+  /**********************************************************************/
+  sum_inj = 0; N_tot = 0;
+  for (k = 0; k < kmax; k++)
+  {
+    N_tot += ge[k] * N_e[k] * delta_ge_int;
+    sum_inj += ge[k] * Qe_inj[k] * delta_ge_int;
+  }
+  N_tot_inj += delta_t * sum_inj;
 
-    fprintf(particle_conservation, "%lf %lf\n", log10(N_tot_inj), log10(N_tot));
-    /**********************************************************************/
-    /**********************************************************************/
+  fprintf(particle_conservation, "%lf %lf\n", log10(N_tot_inj), log10(N_tot));
+  /**********************************************************************/
+  /**********************************************************************/
 
 
 
-    // Prints the interation value every 100 interations.
-    if(i % 100 == 0) 
-    {
-        printf("Finished %d out of %d. \n", i, number_of_elements);
-    }
+  // Prints the interation value every 100 interations.
+  if(i % 100 == 0) 
+  {
+    printf("Finished %d out of %d. \n", i, number_of_elements);
+  }
 }
 
 fclose(nu_L_nu_save); 
