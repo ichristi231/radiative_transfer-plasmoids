377009b1b3e58557c688451564edf96ffdb25c12
Ian Christie <ichristi231@gmail.com>
2019-09-18 15:11:30 -0400

    added physical constant header file

---
+++ physical_constants.h
@@ -0,0 +1,20 @@
+#define PI 3.14159265358979323846 
+
+/* Speed of light in cm/s. */
+#define c 3.0E10
+
+/* Thomson cross section in cm^2. */
+#define sigmaT 6.6524E-25
+
+/* Electron & proton mass in g. */
+#define electron_mass 9.10938E-28
+#define proton_mass 1.6726219E-24
+
+/* Electron charge in CGS. */
+#define electric_charge 4.8032068E-10
+
+/* Plank's constant in erg s. */
+#define plank_const 6.6260755E-27
+
+/* Boltzman constant in erg K^(-1). */
+#define boltzman_const 1.380658E-16
