5ca1b791c3b5cf98c8546db49eb2d087a95654a2
Ian Christie <ichristi231@gmail.com>
2019-11-01 16:42:19 -0500

    add pion mass to physical constants header

--- physical_constants.h
+++ physical_constants.h
@@ -6,9 +6,10 @@
 /* Thomson cross section in cm^2. */
 #define sigmaT 6.6524E-25
 
-/* Electron & proton mass in g. */
+/* Electron, proton, and pion mass in g. */
 #define electron_mass 9.10938E-28
 #define proton_mass 1.6726219E-24
+#define pion_mass 0.146*proton_mass
 
 /* Electron charge in CGS. */
 #define electric_charge 4.8032068E-10
